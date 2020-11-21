29ac793f99645445d23f422a065e77189db3c90e
chutting <tingting.chu@thoughtworks.com>
2020-11-21 12:27:06 +0800

    [Chu] complete p2s2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoSpringConfigApplicationTests {
+	@Autowired private LevelController levelController;
 
 	@Test
-	void contextLoads() {
+	void shouldReturnAdvancedWhenGreaterThanOne() {
+		assertEquals("advanced", levelController.getLevel());
 	}
-
 }
